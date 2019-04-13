35300b3b70c5ef15ab260c78c25d87ea4f6cf312
Suvajit <suvajit.c@outlook.com>
2019-04-13 19:22:18 +0530

    fix: typo

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const Client = require('./src/client/Client');
 
 const client = new Client({ owner: process.env.OWNERS, token: process.env.TOKEN });
 
-client
-	.on('error', err => client.logger.error(`[CLIENT ERROR] ${err.message}`, err.stack))
+client.on('error', err => client.logger.error(`[CLIENT ERROR] ${err.message}`, err.stack))
 	.on('shardError', (err, id) => client.logger.error(`[SHARD ${id} ERROR] ${err.message}`, err.stack))
 	.on('warn', warn => client.logger.warn(`[CLIENT WARN] ${warn}`));
 
