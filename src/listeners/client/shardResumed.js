0d8e3276ea4101bb93f7cbdfe234486284a2ebcb
Suvajit <suvajit.c@outlook.com>
2019-04-13 19:44:32 +0530

    fix: typo

--- src/listeners/client/shardResumed.js
+++ src/listeners/client/shardResumed.js
@@ -10,7 +10,7 @@ class ShardResumeListener extends Listener {
 	}
 
 	exec(id) {
-		this.client.logger.info(`[SHARD ${id} RESUMED] Alright, next time I'll--Eh...again...?`);
+		this.client.logger.info(`[SHARD ${id} RESUMED] Alright, next time I'll--Eh.. again..?`);
 		this.client.promServer.listen(5501);
 		this.client.logger.info(`[SHARD ${id} RESUMED][METRICS] Metrics listening on 5501`);
 	}
